5241b91b6b8876fbc1bd65e4b3d8abe274085bdf
Josh Renaud <josh.renaud41@gmail.com>
2021-08-16 15:03:54 -0500

    added mutations to save and remove books

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,7 +25,45 @@ const LOGIN_USER = gql`
     }
 `;
 
+const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
+
+const SAVE_BOOK = gql`
+    mutation saveBook($book: SavedBookInput!) {
+        saveBook(book: $book) {
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
+
 
 
 export default ADD_USER;
 export default LOGIN_USER;
+export default REMOVE_BOOK;
+export default SAVE_BOOK;
