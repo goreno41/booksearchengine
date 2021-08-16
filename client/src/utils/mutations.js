3c4201d68bfce780cd5def18b7b2e0c2bcad245f
Josh Renaud <josh.renaud41@gmail.com>
2021-08-16 13:46:57 -0500

    added mutation for LOGIN USER and updated loginform to match

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 
-export const ADD_USER = gql`
+const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
             token
@@ -12,3 +12,20 @@ export const ADD_USER = gql`
         }
     }
 `;
+
+const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+
+
+export default ADD_USER;
+export default LOGIN_USER;
