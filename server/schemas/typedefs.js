16faa91e8e631fa6b1e1661ab42af18797af8dbf
Josh Renaud <josh.renaud41@gmail.com>
2021-08-16 14:52:27 -0500

    first iteration of typedefs/resolvers

---
+++ server/schemas/typedefs.js
@@ -0,0 +1,47 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type Query  {
+        me: User
+    }
+
+    type Book {
+        _id: IS
+        authors: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+    }
+    type User {
+        _id: ID
+        ussername: String
+        email: String
+        password: String
+        savedBooks: [Book]
+        bookCount: Int
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    
+    input saveBookInput {
+        description: String
+        title: String
+        bookID: String
+        image: String
+        link: String
+        authors: [String]
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, password: String!, email: String!): Auth
+        saveBook(body: saveBookInput): User
+        removeBook(bookID: String!): User
+    }
+
+`;
+
+module.exports = typeDefs
