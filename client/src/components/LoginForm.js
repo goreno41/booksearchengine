997c995b09500585a87a6644831c159dd689fc76
Josh Renaud <josh.renaud41@gmail.com>
2021-08-16 15:45:15 -0500

    saved books file built

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { LOGIN_USER } from "../utils/mutations"
+import { LOGIN_USER } from "../utils/mutations";
+import { useMutation } from "@apollo/react-hooks";
 
 import Auth from '../utils/auth';
 
---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import gql from "graphql-tag";
+
+const GET_ME = gql` {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export default GET_ME;
