82fcc12f7f8dae152062920bc41fc876a8b90c40
Josh Renaud <josh.renaud41@gmail.com>
2021-08-16 13:40:08 -0500

    added ADD USER mutation and converted signup component to match

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,14 @@
+import gql from "graphql-tag";
+
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                usernam
+            }
+        }
+    }
+`;
